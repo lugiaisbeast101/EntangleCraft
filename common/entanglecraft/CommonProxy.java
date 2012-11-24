084cd5481c12e0cdd19ba480c9c06f6df89f7a3f
MrMasochism <malc.geddes@gmail.com>
2012-11-24 17:07:34 +1300

    fixed gui

--- common/entanglecraft/CommonProxy.java
+++ common/entanglecraft/CommonProxy.java
@@ -16,52 +16,39 @@ import entanglecraft.gui.GuiGenericDestination;
 import entanglecraft.gui.GuiLambdaMiner;
 import net.minecraftforge.common.MinecraftForge;
 
-public class CommonProxy implements IGuiHandler
-{
+public class CommonProxy implements IGuiHandler {
 	public static boolean isServer = true;
-	public void registerClientSide(){
+
+	public void registerClientSide() {
 	}
-	
+
 	public static void registerDistanceSaver(DistanceHandler dh) {
 		MinecraftForge.EVENT_BUS.register(dh);
 	}
-	
-	public File getWorldSaveDir(World world){
+
+	public File getWorldSaveDir(World world) {
 		File workingDir = new File(".\\\\world\\");
 		return workingDir;
 	}
-	
+
 	@Override
-	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
-	  {
+	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
 		if (ID == EnumGui.GenericDestination.getIndex()) {
 			TileEntityGenericDestination tileEntityGD = (TileEntityGenericDestination) world.getBlockTileEntity(x, y, z);
 			return new ContainerGenericDestination(player.inventory, tileEntityGD);
-		
+
 		}
-		
-		else if (ID == EnumGui.LambdaMiner.getIndex()){
+
+		else if (ID == EnumGui.LambdaMiner.getIndex()) {
 			TileEntityLambdaMiner tileEntitylM = (TileEntityLambdaMiner) world.getBlockTileEntity(x, y, z);
 			return new ContainerLambdaMiner(player.inventory, tileEntitylM);
-		}
-		else return null;
-	  }
+		} else
+			return null;
+	}
 
 	@Override
-	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
-	  {
-		
-		if (ID == EnumGui.GenericDestination.getIndex()) {
-			TileEntityGenericDestination tileEntityGD = (TileEntityGenericDestination) world.getBlockTileEntity(x, y, z);
-			return new GuiGenericDestination(player.inventory, tileEntityGD);
-		}
-		
-		else if (ID == EnumGui.LambdaMiner.getIndex()){
-			TileEntityLambdaMiner tileEntitylM = (TileEntityLambdaMiner) world.getBlockTileEntity(x, y, z);
-			return new GuiLambdaMiner(player.inventory, tileEntitylM);
-		}
-		else return null;	
-	  }
-
+	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		return null;
+	}
 
 }
--- minecraft/entanglecraft/ClientProxy.java
+++ minecraft/entanglecraft/ClientProxy.java
@@ -4,24 +4,44 @@ import java.io.File;
 
 import cpw.mods.fml.client.registry.KeyBindingRegistry;
 
+import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.SoundManager;
 import net.minecraft.src.World;
 import entanglecraft.SoundHandling.LambdaSoundHandler;
+import entanglecraft.blocks.TileEntityGenericDestination;
+import entanglecraft.blocks.TileEntityLambdaMiner;
+import entanglecraft.gui.EnumGui;
+import entanglecraft.gui.GuiGenericDestination;
+import entanglecraft.gui.GuiLambdaMiner;
 import net.minecraftforge.common.MinecraftForge;
 
-public class ClientProxy extends CommonProxy{
-	
+public class ClientProxy extends CommonProxy {
+
 	public static boolean isServer = false;
-	
+
 	@Override
 	public void registerClientSide() {
 		MinecraftForge.EVENT_BUS.register(new LambdaSoundHandler());
 		KeyBindingRegistry.registerKeyBinding(new KeyHandling());
 		System.out.println("Tried to register clientside stuff");
 	}
-	
-	public File getWorldSaveDir(World world){
+
+	public File getWorldSaveDir(World world) {
 		File workingDir = new File(".\\\\saves\\" + world.getWorldInfo().getWorldName() + "\\");
 		return workingDir;
+	}
+
+	@Override
+	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		if (ID == EnumGui.GenericDestination.getIndex()) {
+			TileEntityGenericDestination tileEntityGD = (TileEntityGenericDestination) world.getBlockTileEntity(x, y, z);
+			return new GuiGenericDestination(player.inventory, tileEntityGD);
+		}
+
+		else if (ID == EnumGui.LambdaMiner.getIndex()) {
+			TileEntityLambdaMiner tileEntitylM = (TileEntityLambdaMiner) world.getBlockTileEntity(x, y, z);
+			return new GuiLambdaMiner(player.inventory, tileEntitylM);
+		} else
+			return null;
 	}
 }
