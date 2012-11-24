8479f9c5b28e7df0b0ed74827460f1e9222d1a71
MrMasochism <malc.geddes@gmail.com>
2012-11-24 17:22:29 +1300

    fixed key binding firing twice

--- minecraft/entanglecraft/KeyHandling.java
+++ minecraft/entanglecraft/KeyHandling.java
@@ -13,47 +13,45 @@ import cpw.mods.fml.client.registry.KeyBindingRegistry.KeyHandler;
 import cpw.mods.fml.common.TickType;
 
 public class KeyHandling extends KeyHandler {
-    
-    static KeyBinding increment = new KeyBinding("incrementChannel", Keyboard.KEY_G);
 
-    public KeyHandling() {
-            super(new KeyBinding[]{increment}, new boolean[]{false});
-    }
+	static KeyBinding increment = new KeyBinding("incrementChannel", Keyboard.KEY_G);
 
-    @Override
-    public String getLabel() {
-            return "keyBindingsEC";
-    }
+	public KeyHandling() {
+		super(new KeyBinding[] { increment }, new boolean[] { false });
+	}
 
 	@Override
-	public void keyDown(EnumSet<TickType> types, KeyBinding event,
-			boolean tickEnd, boolean isRepeat) {
-		  System.out.println("Trying to change channel");
-	      try {
-	        ItemStack itemInUse = ModLoader.getMinecraftInstance().thePlayer.getCurrentEquippedItem();
-	        if (((itemInUse.getItem() instanceof ItemDevice)) && 
-	          (event == increment)) {
-	          InventoryPlayer inv = ModLoader.getMinecraftInstance().thePlayer.inventory;
-	          int x = itemInUse.stackSize;
-	          inv.setInventorySlotContents(inv.currentItem, new ItemStack(((ItemDevice)itemInUse.getItem()).incrementChannel(), x));
-	          ClientPacketHandler.sendDeviceToggle();
-	          System.out.println("Changed Channel");
-	        } 
-	      }
-	      catch (NullPointerException e)
-	      {
-	        e.printStackTrace();
-	      } 
+	public String getLabel() {
+		return "keyBindingsEC";
+	}
+
+	@Override
+	public void keyDown(EnumSet<TickType> types, KeyBinding event, boolean tickEnd, boolean isRepeat) {
+		if (tickEnd)
+			return;
+		System.out.println("Trying to change channel");
+		try {
+			ItemStack itemInUse = ModLoader.getMinecraftInstance().thePlayer.getCurrentEquippedItem();
+			if (((itemInUse.getItem() instanceof ItemDevice)) && (event == increment)) {
+				InventoryPlayer inv = ModLoader.getMinecraftInstance().thePlayer.inventory;
+				int x = itemInUse.stackSize;
+				inv.setInventorySlotContents(inv.currentItem, new ItemStack(((ItemDevice) itemInUse.getItem()).incrementChannel(), x));
+				ClientPacketHandler.sendDeviceToggle();
+				System.out.println("Changed Channel");
+			}
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void keyUp(EnumSet<TickType> types, KeyBinding kb, boolean tickEnd) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-    public EnumSet<TickType> ticks() {
-        return EnumSet.of(TickType.CLIENT);
-}
+	public EnumSet<TickType> ticks() {
+		return EnumSet.of(TickType.CLIENT);
+	}
 
 }
